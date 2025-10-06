769989e8c1e1e4aaf96d6ce738dcef98f848e7a2
hisham1665 <hishamkhofficial@gmail.com>
2025-10-06 13:28:47 +0530

    completed task 3

--- zoi_test/expense/admin.py
+++ zoi_test/expense/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Expense
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount', 'category', 'date']
+    list_filter = ['category', 'date']
+    search_fields = ['title', 'category__name']
+    date_hierarchy = 'date'
