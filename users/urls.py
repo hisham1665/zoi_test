54fda9aa3dd188139c2cf44eb655a75c68e98d27
hisham1665 <hishamkhofficial@gmail.com>
2025-10-06 14:16:56 +0530

    final commit

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.get_users, name='user_dashboard'),
+    path('users/', views.get_users, name='user_dashboard'),
     path('users/<int:user_id>/', views.get_user_detail, name='user_detail'),
     path('users/<int:user_id>/delete/', views.delete_user, name='delete_user'),
 ]
